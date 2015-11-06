cf8aa9af16ec4b8983cd98360a16b349a65f9d63
Leandro Vega <leandrovega@ubuntu.(none)>
2015-11-06 05:03:20 -0300

    traceroute funciona bien!!!

--- traceroute.py
+++ traceroute.py
@@ -1,9 +1,34 @@
 #! /usr/bin/env python
-import sys
+from __future__ import with_statement # Required in 2.5
+from contextlib import contextmanager
+import sys, os
 import time
 from scapy.all import *
-import multiprocessing
 import signal
+
+
+class TimeoutException(Exception): pass
+
+@contextmanager
+def time_limit(seconds):
+    def signal_handler(signum, frame):
+        raise TimeoutException, "Timed out!"
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(seconds)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
+
+@contextmanager
+def suppress_stdout():
+	with open(os.devnull, "w") as devnull:
+		old_stdout = sys.stdout
+		sys.stdout = devnull
+		try:  
+			yield
+		finally:
+			sys.stdout = old_stdout
 
 
 if __name__ == '__main__':
@@ -11,19 +36,31 @@ if __name__ == '__main__':
 	if len(sys.argv) != 1:
 		assert("Error")
 
-	h = sys.argv[0] #Duda
+	host = sys.argv[1]
 	ttl = 1
 	hayEchoReplay = False
 
 	while not(hayEchoReplay):
-		t0 = time.time()
-		res = sr(IP(dst='www.google.com', ttl=ttl) / ICMP())
-		t1 = time.time()
-		rtt = t1 - t0
-		print(rtt)
-		if res[0][0][1][1].type == 11:
-			print(res[0][ICMP][0][1][0].src)
-		if res[0][0][1][1].type == 0:
-			hayEchoReplay = True
-		ttl = ttl + 1
 
+		try:
+			with time_limit(5):
+				with suppress_stdout():
+					t0 = time.time()
+					res = sr(IP(dst=host, ttl=ttl) / ICMP())
+					t1 = time.time()
+					rtt = t1 - t0
+				TimeOut = False
+		except TimeoutException, msg:
+			TimeOut = True
+
+		if not TimeOut:
+			if res[0][0][1][1].type == 11:
+				print 'TTL: %d' %ttl, '    RTT: %s' %rtt, '    IP Source: %s' %res[0][ICMP][0][1][0].src
+			if res[0][0][1][1].type == 0:
+				hayEchoReplay = True
+				print 'TTL: %d' %ttl, '    RTT: %s' %rtt, '    IP Source: %s' %res[0][ICMP][0][1][0].src
+
+		if TimeOut:
+			print 'TTL: %d' %ttl, '    Time Out!'
+
+		ttl = ttl + 1
