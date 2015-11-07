4637dd5e749a5d0d161502f6a694ce2a44798af8
pepepotamoo <rodricis@hotmail.com>
2015-11-06 21:17:39 -0300

    punto a y b finish

--- traceroute.py
+++ traceroute.py
@@ -5,6 +5,9 @@ import sys, os
 import time
 from scapy.all import *
 import signal
+import math
+import scipy.stats as stats
+
 
 
 class TimeoutException(Exception): pass
@@ -33,34 +36,54 @@ def suppress_stdout():
 
 if __name__ == '__main__':
 
-	if len(sys.argv) != 1:
+	if len(sys.argv) != 2:
 		assert("Error")
 
 	host = sys.argv[1]
+	timeLimit = int(sys.argv[2]) 
 	ttl = 1
 	hayEchoReplay = False
+	rttA = 0
+	array = []
 
 	while not(hayEchoReplay):
 
 		try:
-			with time_limit(5):
+			with time_limit(timeLimit):
 				with suppress_stdout():
-					t0 = time.time()
 					res = sr(IP(dst=host, ttl=ttl) / ICMP())
-					t1 = time.time()
-					rtt = t1 - t0
 				TimeOut = False
 		except TimeoutException, msg:
 			TimeOut = True
 
+		rtt = 0
+		rtt2 = 0
+
 		if not TimeOut:
+			for i in range(0,10):
+				with suppress_stdout():
+					t0 = time.time()
+					sr(IP(dst=host, ttl=ttl) / ICMP())
+					t1 = time.time()
+				rtt = rtt + t1-t0
+				rtt2 = rtt2 + (t1-t0)**2
+			rttSuma2Prom = rtt2/10
+			rttProm = rtt/10
+			array.append(rttProm-rttA)
+			rttProm2 = rttProm**2
+			varianza = rttSuma2Prom - rttProm2
+			desvioStandar = math.sqrt(varianza)
+
 			if res[0][0][1][1].type == 11:
-				print 'TTL: %d' %ttl, '    RTT: %s' %rtt, '    IP Source: %s' %res[0][ICMP][0][1][0].src
+				print 'TTL: %d' %ttl, '    RTT: %s' %rttProm, '		DS: %s' %desvioStandar, '		RTTResta: %s' %(rttProm-rttA), '    IP Source: %s' %res[0][ICMP][0][1][0].src
 			if res[0][0][1][1].type == 0:
 				hayEchoReplay = True
-				print 'TTL: %d' %ttl, '    RTT: %s' %rtt, '    IP Source: %s' %res[0][ICMP][0][1][0].src
+				print 'TTL: %d' %ttl, '    RTT: %s' %rttProm, '		DS: %s' %desvioStandar, '		RTTResta: %s' %(rttProm-rttA), '    IP Source: %s' %res[0][ICMP][0][1][0].src
+			rttA = rttProm
 
 		if TimeOut:
 			print 'TTL: %d' %ttl, '    Time Out!'
 
 		ttl = ttl + 1
+
+	print stats.normaltest(array, axis=0)
